331056fc87bc8fc25ecd8c505ded302b5a285e6d
DavidNNussbaum <brocknynj@aol.com>
2021-03-24 18:31:13 -0400

    commit tree 6fc9ce2135e9bc262b57b94374781af06c0036ce

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20190418213203) do
 
-  create_table "authors", force: :cascade do |t|
-    t.string   "name"
-    t.string   "hometown"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.string   "description"
